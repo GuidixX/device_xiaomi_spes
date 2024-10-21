a4a04852bdeb05594629ab663cd10ea02d73473c
Guidix <guidixm@gmail.com>
2024-10-21 15:05:23 +0300

    spes: Update libinit from topaz

--- libinit/init_spes.cpp
+++ libinit/init_spes.cpp
@@ -14,21 +14,22 @@
  * limitations under the License.
  */
 
-#include <cstdlib>
-#include <string.h>
+#include <vector>
 
+#include <android-base/logging.h>
+#include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
 #include <sys/sysinfo.h>
-#include <android-base/properties.h>
-
-#include "property_service.h"
-#include "vendor_init.h"
 
 using android::base::GetProperty;
-using std::string;
 
-void property_override(string prop, string value)
+// list of partitions to override props
+std::vector<std::string> ro_props_default_source_order = {
+    "", "odm.", "odm_dlkm.", "product.", "system.", "system_ext.", "vendor.", "vendor_dlkm.",
+};
+
+void property_override(std::string prop, std::string value)
 {
     auto pi = (prop_info*) __system_property_find(prop.c_str());
 
@@ -38,64 +39,84 @@ void property_override(string prop, string value)
         __system_property_add(prop.c_str(), prop.size(), value.c_str(), value.size());
 }
 
-void load_dalvik_properties(void) {
-        struct sysinfo sys;
+void load_redmi_spes() {
+    property_override("ro.product.mod_device", "spes_global");
+}
 
-        sysinfo(&sys);
-        if (sys.totalram < 4096ull * 1024 * 1024) {
-        // from - phone-xhdpi-4096-dalvik-heap.mk
-        property_override("dalvik.vm.heapstartsize", "8m");
-        property_override("dalvik.vm.heapgrowthlimit", "192m");
-        property_override("dalvik.vm.heapmaxfree", "16m");
-	    property_override("dalvik.vm.heaptargetutilization", "0.6");
+void load_redmi_spes_in() {
+    property_override("ro.product.device", "spes");
+    property_override("ro.product.model", "Redmi Note 11");
+    property_override("ro.product.name", "spes_in");
+    property_override("ro.product.mod_device", "spes_in_global");
+}
+
+void load_redmi_spesn() {
+    property_override("ro.product.device", "spesn");
+    property_override("ro.product.model", "Redmi Note 11 NFC");
+    property_override("ro.product.name", "spesn_global");
+    property_override("ro.product.mod_device", "spesn_global");
+}
+
+void vendor_load_properties() {
+    std::string hwname = GetProperty("ro.boot.hwname", "");
+    std::string region = GetProperty("ro.boot.hwc", "");
+    std::string hwversion = GetProperty("ro.boot.hwversion", "");
+
+    if (access("/system/bin/recovery", F_OK) != 0) {
+        if (hwname == "spesn") {
+           load_redmi_spesn();
+        } else if (hwname == "spes") {
+        if (region == "India") {
+           load_redmi_spes_in();
+        } else {
+           load_redmi_spes();
         }
     }
-
-void set_ro_build_prop(const string &source, const string &prop,
-                       const string &value, bool product = false) {
-    string prop_name;
-
-    if (product)
-        prop_name = "ro.product." + source + prop;
-    else
-        prop_name = "ro." + source + "build." + prop;
-
-    property_override(prop_name.c_str(), value.c_str());
 }
-
-void set_device_props(const string model, const string name, const string marketname,
-                      const string mod_device) {
-    // list of partitions to override props
-    string source_partitions[] = { "", "bootimage.", "product.""system.",
-                                   "system_ext.", "vendor." };
-
-    for (const string &source : source_partitions) {
-        set_ro_build_prop(source, "model", model, true);
-        set_ro_build_prop(source, "name", name, true);
-        set_ro_build_prop(source, "marketname", marketname, true);
-    }
-    property_override("ro.product.mod_device", mod_device.c_str());
-    property_override("bluetooth.device.default_name", marketname.c_str());
-    property_override("vendor.usb.product_string", marketname.c_str());
-}
-
-void vendor_load_properties()
-{
-    // Detect device and configure properties
-
-    if (GetProperty("ro.boot.hwname", "") == "spesn") { // Redmi Note 11 NFC
-        set_device_props("2201117TY", "spesn_global", "Redmi Note 11 NFC", "spesn_global");
-    } else { // Redmi Note 11
-        string region = GetProperty("ro.boot.hwc", "");
-        if (region == "IN") // Redmi Note 11 India
-            set_device_props("2201117TI", "spes_in", "Redmi Note 11", "spes_in_global");
-        else // Redmi Note 11 Global
-            set_device_props("2201117TG", "spes_global", "Redmi Note 11", "spes_global");
-    }
 
     // Set hardware revision
     property_override("ro.boot.hardware.revision", GetProperty("ro.boot.hwversion", "").c_str());
 
+    // Override "release-keys" for build tags
+    property_override("ro.build.tags", "release-keys");
+
     // Set dalvik heap configuration
-    load_dalvik_properties();
+    std::string heapstartsize, heapgrowthlimit, heapsize, heapminfree,
+			heapmaxfree, heaptargetutilization;
+
+    struct sysinfo sys;
+    sysinfo(&sys);
+
+    if (sys.totalram > 5072ull * 1024 * 1024) {
+        // from - phone-xhdpi-6144-dalvik-heap.mk
+        heapstartsize = "16m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heaptargetutilization = "0.5";
+        heapminfree = "8m";
+        heapmaxfree = "32m";
+    } else if (sys.totalram > 3072ull * 1024 * 1024) {
+        // from - phone-xhdpi-4096-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heaptargetutilization = "0.6";
+        heapminfree = "8m";
+        heapmaxfree = "16m";
+    } else {
+        // from - phone-xhdpi-2048-dalvik-heap.mk
+        heapstartsize = "8m";
+        heapgrowthlimit = "192m";
+        heapsize = "512m";
+        heaptargetutilization = "0.75";
+        heapminfree = "512k";
+        heapmaxfree = "8m";
+    }
+
+    property_override("dalvik.vm.heapstartsize", heapstartsize);
+    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+    property_override("dalvik.vm.heapsize", heapsize);
+    property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
+    property_override("dalvik.vm.heapminfree", heapminfree);
+    property_override("dalvik.vm.heapmaxfree", heapmaxfree);
 }
