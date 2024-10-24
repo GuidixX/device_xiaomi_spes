GuidixX/device_xiaomi_spes_

spes: Sign Build With custom Signing Keys

device.mk
@@ -498,6 +498,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.servicetracker@1.2.vendor
 
+# Signing
+-include vendor/lineage/signing/keys/keys.mk
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \

GuidixX/device_xiaomi_spes

device.mk
@@ -467,6 +467,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
+# Signing
+-include vendor/yaap/signing/keys/keys.mk
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
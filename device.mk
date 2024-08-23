8aa5930019ae28247fae55e8eac2f9ee8f7eaf74
Guidix <guidixm@gmail.com>
2024-08-23 10:09:16 +0300

    spes: Sign Build With custom Signing Keys

--- device.mk
+++ device.mk
@@ -467,6 +467,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
+# Signing
+-include vendor/yaap/signing/keys/keys.mk
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
